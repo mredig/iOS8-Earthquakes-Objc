fac8bff391c610a61df10a01bfade1aac2a8d1b2
Michael Redig <mredig@gmail.com>
2019-10-08 11:48:13 -0500

    added earthquake view controller

---
+++ iOS8-Earthquakes-Objc/iOS8-Earthquakes-Objc/EarthquakesViewController.h
@@ -0,0 +1,17 @@
+//
+//  EarthquakesViewController.h
+//  iOS8-Quakes-Objc
+//
+//  Created by Paul Solt on 10/8/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EarthquakesViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ iOS8-Earthquakes-Objc/iOS8-Earthquakes-Objc/EarthquakesViewController.m
@@ -0,0 +1,28 @@
+//
+//  EarthquakesViewController.m
+//  iOS8-Quakes-Objc
+//
+//  Created by Paul Solt on 10/8/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import "EarthquakesViewController.h"
+#import <MapKit/MapKit.h>
+
+@interface EarthquakesViewController ()
+
+//@IBOutlet var mapView: MKMapView!  // Swift
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
+
+
+@end
+
+@implementation EarthquakesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+}
+
+
+@end
