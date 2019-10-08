ae03e7b6cc14cd05486930b02c50b88110851818
Michael Redig <mredig@gmail.com>
2019-10-08 12:24:00 -0500

    added file assistant

--- iOS8-Earthquakes-Objc/iOS8-Earthquakes-ObjcTests/LSIQuakesTests.m
+++ iOS8-Earthquakes-Objc/iOS8-Earthquakes-ObjcTests/LSIQuakesTests.m
@@ -6,12 +6,33 @@
 //  Copyright © 2019 Red_Egg Productions. All rights reserved.
 //
 
+
 #import <XCTest/XCTest.h>
+#import "LSIFileHelper.h"
 
 @interface LSIQuakesTests : XCTestCase
 
 @end
 
 @implementation LSIQuakesTests
+
+- (void)testFileHelper {
+	NSBundle* bundle = [NSBundle bundleForClass:[self class]];
+	NSData* data = loadFile(@"Quake.json", bundle);
+
+	XCTAssertNotNil(data);
+	NSLog(@"Data: %@", data);
+}
+
+- (void)testParseQuakeJSON {
+	NSBundle* bundle = [NSBundle bundleForClass:[self class]];
+	NSData* data = loadFile(@"Quake.json", bundle);
+
+	NSError* error = nil;
+	NSDictionary* json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+
+	NSLog(@"Error: %@", error);
+	NSLog(@"JSON: %@", json);
+}
 
 @end
